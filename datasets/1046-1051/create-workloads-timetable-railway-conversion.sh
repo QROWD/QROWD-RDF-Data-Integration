0c782ed00e725abb831f74864fcfed45f847b8b9
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-03-07 00:55:06 +0100

    changed the workload / transformation separation to meet my high demands :)

---
+++ datasets/1046-1051/create-workloads-timetable-railway-conversion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+COUNTER=0
+# https://stackoverflow.com/questions/2596462/how-to-strip-leading-in-unix-find
+for f in $(find -name '*.json' | sed "s|^\./||"); do 
+  echo "INSERT {
+  eg:workload"$COUNTER" eg:workload ?o
+  }
+  WHERE {
+  <"$f"> url:text ?src .
+  BIND(STRDT(?src, xsd:json) AS ?o)
+  }"
+  let COUNTER=COUNTER+1 
+done
+#sparql-integrate --sparql=workload.sparql --sparql=timetable-railway-conversion.sparql
+#rm workload.sparql
+
+
