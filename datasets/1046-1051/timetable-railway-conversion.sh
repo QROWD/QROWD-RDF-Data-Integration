7b0ee03892870b860bb7bb787725351e1528201b
BonaBeavis <gordian.dziwis@gmail.com>
2018-03-06 17:38:20 +0100

    Improves workload script for datasets 1046-1051

--- datasets/1046-1051/timetable-railway-conversion.sh
+++ datasets/1046-1051/timetable-railway-conversion.sh
@@ -1,14 +1,16 @@
-for f in $(find -name '*.json');
-do echo "INSERT {
-  eg:workload1 eg:workload ?o
+#!/bin/bash
+COUNTER=0
+for f in $(find -name '*.json'); do 
+  echo "INSERT {
+  eg:workload"$COUNTER" eg:workload ?o
   }
   WHERE {
   <"$f"> url:text ?src .
   BIND(STRDT(?src, xsd:json) AS ?o)
-  }" >> workload.sparql;
-  sparql-integrate --sparql=workload.sparql --sparql=timetable-railway-conversion.sparql
-  rm workload.sparql
+  }" >> workload.sparql
+  let COUNTER=COUNTER+1 
 done
-
+sparql-integrate --sparql=workload.sparql --sparql=timetable-railway-conversion.sparql
+rm workload.sparql
 
 
