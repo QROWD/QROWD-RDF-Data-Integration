caa808eda8dd32e27d2e0d20b0082a5b0642193e
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-03-08 22:04:21 +0100

    cleanup

--- datasets/1046-1051/create-workloads.sh
+++ datasets/1046-1051/create-workloads.sh
@@ -4,11 +4,13 @@ COUNTER=0
 for f in $(find -name '*.json' | sed "s|^\./||"); do 
   echo "INSERT {
   eg:workload"$COUNTER" eg:workload ?o
-  }
-  WHERE {
+}
+WHERE {
   <"$f"> url:text ?src .
   BIND(STRDT(?src, xsd:json) AS ?o)
-  }"
+}
+
+"
   let COUNTER=COUNTER+1 
 done
 #sparql-integrate --sparql=workload.sparql --sparql=timetable-railway-conversion.sparql
