9a83529eebc3e5676bb77560cbcad39a051873c3
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-04-03 18:20:15 +0200

    Towards creating self describing CKAN datasets

--- datasets/1046-1051/create-workloads.sh
+++ datasets/1046-1051/create-workloads.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 COUNTER=0
 # https://stackoverflow.com/questions/2596462/how-to-strip-leading-in-unix-find
+
+#
+
+echo "
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+INSERT DATA{
+  eg:trentoRailwayTimeTablesDataset
+    a dcat:Dataset ;
+    dct:identifier \"trento-railway-time-tables\" ;
+    dct:title \"Trento Railway Timetables\" ;
+    dct:description \"Timetables\" ;
+    .
+}
+
+"
+
 for f in $(find -name '*.json' | sed "s|^\./||"); do 
-  echo "INSERT {
-  eg:workload"$COUNTER" eg:workload ?o
+# TODO derive name - remove file extension, replace / with -
+
+  echo "
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+INSERT {
+  eg:trentoRailwayTimeTablesDataset
+    dcat:distribution eg:resource${COUNTER} ;
+    .  
+
+  eg:resource${COUNTER}
+    a dcat:Distribution ;
+    dct:identifier \"$f\" ;
+    rdfs:label \"TODO label\" ;
+    rdfs:comment \"TODO comment\" ;
+    eg:workload ?o ;
+    .
+
 }
 WHERE {
-  <"$f"> url:text ?src .
+  <$f> url:text ?src .
   BIND(STRDT(?src, xsd:json) AS ?o)
 }
 
