3f4b6c69d7978a40410eeb75c8f4aad80af209fb
BonaBeavis <gordian.dziwis@gmail.com>
2018-04-13 16:13:28 +0200

    Updates weather dataset

---
+++ datasets/weather-data/create-workloads.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+DATASET=$(basename "$(pwd)")
+DATASET_CAMEL_CASE=$(echo "$DATASET" | sed -r 's/(^|-)(\w)/\U\2/g')
+DATASET_TITLE=$(echo "$DATASET" | sed -r 's/(^|-)(\w)/\U \2/g')
+
+echo "
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+INSERT DATA{
+  eg:"$DATASET_CAMEL_CASE"Dataset
+    a dcat:Dataset ;
+    dct:identifier \"$DATASET\" ;
+    dct:title \"$DATASET_TITLE\" ;
+    dct:description \"Stuff\" ;
+    dcat:keyword \"Keyword\", \"Keyword\" ;
+    .
+}
+"
+
+for DISTRIBUTION in distributions/*; do
+  DISTRIBUTION_NAME=$(basename $DISTRIBUTION)
+  echo "
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX aws: <http://purl.oclc.org/NET/ssnx/meteo/aws/>
+INSERT {
+  eg:"$DATASET_CAMEL_CASE"Dataset
+    dcat:distribution eg:resource-$DISTRIBUTION_NAME ;
+    .  
+
+  eg:resource-$DISTRIBUTION_NAME
+    a dcat:Distribution ;
+#   dct:identifier \"$DISTRIBUTION_NAME\" ;
+    dct:title \"$DISTRIBUTION_NAME\" ;
+    dct:description \"RDF dataset with timetables for $DISTRIBUTION_NAME\" ;
+# Hijacking the dcat namespace to refer to a set of graphs within this dataset
+    dcat:accessURL eg:graph-$DISTRIBUTION_NAME ;
+    .
+
+  eg:workload-$DISTRIBUTION_NAME
+    eg:workload ?workload ;
+    eg:resultGraph eg:graph-$DISTRIBUTION_NAME ;
+    aws:TemperatureSensor <http://qrowd-project.eu/resource/$DISTRIBUTION_NAME-sensor> ;
+    .
+
+}
+WHERE {
+  <$DISTRIBUTION> url:text ?workload .
+}
+"
+done
